6ba5666de86690d486a1d41f19205011d031939d
Estefania Tastan <etastan@siaaccess.com>
2017-12-15 17:00:11 -0500

    Modifying how the datatable content renders

--- src/components/modules/datatable.js
+++ src/components/modules/datatable.js
@@ -7,33 +7,6 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 import {BootstrapTable, TableHeaderColumn, SizePerPageDropDown} from 'react-bootstrap-table';
 
-export class Filter extends Component {
-    constructor() {
-      super();
-      this.state = {
-          data: {},
-          companies: {},
-          employeeName: ''
-      };
-    }
-
-    render() {
-
-        return (
-
-                <div className="wrapper wrapper__content--whiteBox">
-                    <form className="form">
-                        <span className="tag--text">Filter by</span>
-                        <span className="tag tag--inactive">Cheques</span>
-                        <span className="tag tag--active">Purchases</span>
-                        <span className="tag tag--inactive">Balance</span>
-                        <span className="tag tag--active">All</span>
-                    </form>
-                </div>
-        );
-    }
-}
-
 export class DataTable extends Component {
 
     constructor() {
@@ -41,12 +14,15 @@ export class DataTable extends Component {
       this.state = {
           accountData: {},
           companies: {},
-          employeeName: ''
+          employeeName: '',
+          pagination: 'pagination'
       };
     }
 
     componentDidMount() {
         var accounts = {};
+        var pagination = document.getElementsByClassName(this.state.pagination);
+
 
         /*
          * Obtain DataTable info and store in the component's state.
@@ -64,7 +40,23 @@ export class DataTable extends Component {
                 console.error(this.props.url, status, err.toString());
             }.bind(this)
         });
+
+        /*
+         * We must adjust the positioning of our pagination element post-mount, unfortunately
+         * this cannot be customized with the current version of BootStrap table (version 4.0)
+         */
+
+         for (var i = 0; i < pagination.length; i++) {
+             if (pagination[i].nodeName == 'UL') {
+                 $(pagination[i].parentNode).detach().appendTo('#paginationContainer');
+
+             }
+         }
+
+
     }
+
+
 
     render() {
         var accounts = this.state.accountData.length ? this.state.accountData : [];
@@ -72,9 +64,11 @@ export class DataTable extends Component {
         const options = {
             page: 1,  // which page you want to show as default
             sizePerPageList: [ {
-            text: '5', value: 5
-            }, {
             text: '10', value: 10
+            }, {
+            text: '25', value: 25
+            }, {
+            text: '50', value: 50
             }, {
             text: 'All', value: accounts.length
             } ], // you can change the dropdown list for size per page
@@ -92,20 +86,20 @@ export class DataTable extends Component {
         return (
 
             <section className="wrapper wrapper__content wrapper__content--inner">
-                <Filter/>
                 <div className="wrapper wrapper__content--whiteBox">
                     <BootstrapTable data={accounts} options={options} striped hover pagination tableHeaderClass='dataTable__row--header' trClassName='dataTable__row--content'>
-                         <TableHeaderColumn dataSort={ true } filter={ { type: 'TextFilter' } } isKey dataField='id'>ID</TableHeaderColumn>
-                         <TableHeaderColumn dataSort={ true } filter={ { type: 'TextFilter'} } dataField='supplier'>Supplier</TableHeaderColumn>
-                         <TableHeaderColumn dataSort={ true } filter={ { type: 'TextFilter'} } dataField='address'>Address</TableHeaderColumn>
-                         <TableHeaderColumn dataSort={ true } dataField='city'>City</TableHeaderColumn>
-                         <TableHeaderColumn dataSort={ true } dataField='province'>Province</TableHeaderColumn>
-                         <TableHeaderColumn dataSort={ true } dataField='telephone'>Telephone</TableHeaderColumn>
-                         <TableHeaderColumn dataSort={ true } dataField='balanceDue'>Balance Due</TableHeaderColumn>
-                         <TableHeaderColumn dataSort={ true } dataField='lastInvoice'>Last Invoice</TableHeaderColumn>
-                         <TableHeaderColumn dataSort={ true } dataField='lastCheque'>Last Cheque</TableHeaderColumn>
-                         <TableHeaderColumn dataSort={ true } dataField='currentPer'>Current Per</TableHeaderColumn>
+                         <TableHeaderColumn width='100' dataSort={ true } filter={ { type: 'TextFilter' } } isKey dataField='id'>ID</TableHeaderColumn>
+                         <TableHeaderColumn width='175' dataSort={ true } filter={ { type: 'TextFilter'} } dataField='supplier'>Supplier</TableHeaderColumn>
+                         <TableHeaderColumn width='175' dataSort={ true } filter={ { type: 'TextFilter'} } dataField='address'>Address</TableHeaderColumn>
+                         <TableHeaderColumn width='100' dataSort={ true } dataField='city'>City</TableHeaderColumn>
+                         <TableHeaderColumn width='100' dataSort={ true } dataField='province'>Province</TableHeaderColumn>
+                         <TableHeaderColumn width='125' dataSort={ true } dataField='telephone'>Telephone</TableHeaderColumn>
+                         <TableHeaderColumn width='125' dataSort={ true } dataField='balanceDue'>Balance Due</TableHeaderColumn>
+                         <TableHeaderColumn width='125' dataSort={ true } dataField='lastInvoice'>Last Invoice</TableHeaderColumn>
+                         <TableHeaderColumn width='125' dataSort={ true } dataField='lastCheque'>Last Cheque</TableHeaderColumn>
+                         <TableHeaderColumn width='125' dataSort={ true } dataField='currentPer'>Current Per</TableHeaderColumn>
                      </BootstrapTable>
+                     <div id="paginationContainer" className="dataTable__pagination"></div>
                 </div>
 
             </section>
